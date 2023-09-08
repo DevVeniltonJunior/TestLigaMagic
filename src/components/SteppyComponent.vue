138a12ee4e28dcd06233503ade23e8dd962e844d
Venilton Junior <venilton.junior.dev@gmail.com>
2023-09-08 17:14:26 -0300

    feat: create Stepper component

---
+++ src/components/SteppyComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-for="(step, index) in steps" :key="index" :class="{ 'step-active': index === currentStep }">
+      {{ step }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    steps: Array, // Um array de strings representando cada etapa
+    currentStep: Number // O número da etapa atual
+  }
+};
+</script>
+
+<style>
+.step-active {
+  font-weight: bold;
+  color: blue;
+}
+</style>
